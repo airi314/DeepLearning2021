13cdfe713b8745ff1cc95bb375eaa01ac7854a72
airi314 <olka.wichrowska@gmail.com>
2021-04-27 09:57:23 +0200

    update models definition

--- Project_3/models.py
+++ Project_3/models.py
@@ -2,25 +2,36 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
-class M3(nn.Module):
-    def __init__(self, n_input=1, n_output=35, stride=4, n_channel=256):
+class Block(nn.Module):
+    def __init__(self, n_input=1, n_channel=64, kernel_size = 3, stride=1, repeat = 1):
         super().__init__()
-        self.conv1 = nn.Conv1d(
-            n_input, n_channel, kernel_size=80, stride=stride)
-        self.bn1 = nn.BatchNorm1d(n_channel)
-        self.pool1 = nn.MaxPool1d(4)
-        self.conv2 = nn.Conv1d(n_channel, n_channel, kernel_size=3)
-        self.bn2 = nn.BatchNorm1d(n_channel)
-        self.pool2 = nn.MaxPool1d(4)
+
+        layers = [nn.Conv1d(n_input, n_channel, kernel_size, stride)]
+        for i in range(repeat - 1):
+            layers.append(nn.Conv1d(n_channel, n_channel, kernel_size, stride))
+        layers += [nn.BatchNorm1d(n_channel),
+            nn.ReLU(),
+            nn.MaxPool1d(4)]
+
+        self.block = nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = self.block(x)
+        return x
+
+
+class M3(nn.Module):
+    def __init__(self, n_input=1, n_output=31, n_channel=256):
+        super().__init__()
+
+        self.model = nn.Sequential(
+            Block(n_input, n_channel, 80, 4),
+            Block(n_channel, n_channel, 3)
+        )
         self.fc1 = nn.Linear(n_channel, n_output)
 
     def forward(self, x):
-        x = self.conv1(x)
-        x = F.relu(self.bn1(x))
-        x = self.pool1(x)
-        x = self.conv2(x)
-        x = F.relu(self.bn2(x))
-        x = self.pool2(x)
+        x = self.model(x)
         x = F.avg_pool1d(x, x.shape[-1])
         x = x.permute(0, 2, 1)
         x = self.fc1(x)
@@ -28,36 +39,19 @@ class M3(nn.Module):
 
 
 class M5(nn.Module):
-    def __init__(self, n_input=1, n_output=35, stride=4, n_channel=128):
+    def __init__(self, n_input=1, n_output=31, n_channel=128):
         super().__init__()
-        self.conv1 = nn.Conv1d(
-            n_input, n_channel, kernel_size=80, stride=stride)
-        self.bn1 = nn.BatchNorm1d(n_channel)
-        self.pool1 = nn.MaxPool1d(4)
-        self.conv2 = nn.Conv1d(n_channel, n_channel, kernel_size=3)
-        self.bn2 = nn.BatchNorm1d(n_channel)
-        self.pool2 = nn.MaxPool1d(4)
-        self.conv3 = nn.Conv1d(n_channel, 2 * n_channel, kernel_size=3)
-        self.bn3 = nn.BatchNorm1d(2 * n_channel)
-        self.pool3 = nn.MaxPool1d(4)
-        self.conv4 = nn.Conv1d(2 * n_channel, 4 * n_channel, kernel_size=3)
-        self.bn4 = nn.BatchNorm1d(4 * n_channel)
-        self.pool4 = nn.MaxPool1d(4)
-        self.fc1 = nn.Linear(4 * n_channel, n_output)
+
+        self.model = nn.Sequential(
+            Block(n_input, n_channel, 80, 4),
+            Block(n_channel, n_channel, 3),
+            Block(n_channel, 2*n_channel, 3),
+            Block(2* n_channel, 4*n_channel, 3)
+        )
+        self.fc1 = nn.Linear(4*n_channel, n_output)
 
     def forward(self, x):
-        x = self.conv1(x)
-        x = F.relu(self.bn1(x))
-        x = self.pool1(x)
-        x = self.conv2(x)
-        x = F.relu(self.bn2(x))
-        x = self.pool2(x)
-        x = self.conv3(x)
-        x = F.relu(self.bn3(x))
-        x = self.pool3(x)
-        x = self.conv4(x)
-        x = F.relu(self.bn4(x))
-        x = self.pool4(x)
+        x = self.model(x)
         x = F.avg_pool1d(x, x.shape[-1])
         x = x.permute(0, 2, 1)
         x = self.fc1(x)
@@ -65,52 +59,43 @@ class M5(nn.Module):
 
 
 class M11(nn.Module):
-    def __init__(self, n_input=1, n_output=35, stride=4, n_channel=64):
+    def __init__(self, n_input=1, n_output=31, n_channel=64):
         super().__init__()
-        self.conv1 = nn.Conv1d(
-            n_input, n_channel, kernel_size=80, stride=stride)
-        self.bn1 = nn.BatchNorm1d(n_channel)
-        self.pool1 = nn.MaxPool1d(4)
-        self.conv2 = nn.Conv1d(n_channel, n_channel, kernel_size=3)
-        self.conv2_2 = nn.Conv1d(n_channel, n_channel, kernel_size=3)
-        self.bn2 = nn.BatchNorm1d(n_channel)
-        self.pool2 = nn.MaxPool1d(4)
-        self.conv3 = nn.Conv1d(n_channel, 2 * n_channel, kernel_size=3)
-        self.conv3_2 = nn.Conv1d(2 * n_channel, 2 * n_channel, kernel_size=3)
-        self.bn3 = nn.BatchNorm1d(2 * n_channel)
-        self.pool3 = nn.MaxPool1d(4)
-        self.conv4 = nn.Conv1d(2 * n_channel, 4 * n_channel, kernel_size=3)
-        self.conv4_2 = nn.Conv1d(4 * n_channel, 4 * n_channel, kernel_size=3)
-        self.conv4_3 = nn.Conv1d(4 * n_channel, 4 * n_channel, kernel_size=3)
-        self.bn4 = nn.BatchNorm1d(4 * n_channel)
-        self.pool4 = nn.MaxPool1d(4)
 
-        self.conv5 = nn.Conv1d(4 * n_channel, 8 * n_channel, kernel_size=3)
-        self.conv5_2 = nn.Conv1d(8 * n_channel, 8 * n_channel, kernel_size=3)
-        self.bn5 = nn.BatchNorm1d(8 * n_channel)
+        self.model = nn.Sequential(
+            Block(n_input, n_channel, 80, 4),
+            Block(n_channel, n_channel, 3, 1, 2),
+            Block(n_channel, 2* n_channel, 3, 1, 2),
+            Block(2*n_channel, 4* n_channel, 3, 1, 3),
+            Block(4*n_channel, 8* n_channel, 3, 1, 2),
+            )
 
         self.fc1 = nn.Linear(8 * n_channel, n_output)
 
     def forward(self, x):
-        x = self.conv1(x)
-        x = F.relu(self.bn1(x))
-        x = self.pool1(x)
-        x = self.conv2(x)
-        x = self.conv2_2(x)
-        x = F.relu(self.bn2(x))
-        x = self.pool2(x)
-        x = self.conv3(x)
-        x = self.conv3_2(x)
-        x = F.relu(self.bn3(x))
-        x = self.pool3(x)
-        x = self.conv4(x)
-        x = self.conv4_2(x)
-        x = self.conv4_3(x)
-        x = F.relu(self.bn4(x))
-        x = self.pool4(x)
-        x = self.conv5(x)
-        x = self.conv5_2(x)
-        x = F.relu(self.bn5(x))
+        x = self.model(x)
+        x = F.avg_pool1d(x, x.shape[-1])
+        x = x.permute(0, 2, 1)
+        x = self.fc1(x)
+        return F.log_softmax(x, dim=2)
+
+
+class M18(nn.Module):
+    def __init__(self, n_input=1, n_output=31, n_channel=64):
+        super().__init__()
+
+        self.model = nn.Sequential(
+            Block(n_input, n_channel, 80, 4),
+            Block(n_channel, n_channel, 3, 1, 4),
+            Block(n_channel, 2* n_channel, 3, 1, 4),
+            Block(2*n_channel, 4* n_channel, 3, 1, 4),
+            Block(4*n_channel, 8* n_channel, 3, 1, 4),
+            )
+
+        self.fc1 = nn.Linear(8 * n_channel, n_output)
+
+    def forward(self, x):
+        x = self.model(x)
         x = F.avg_pool1d(x, x.shape[-1])
         x = x.permute(0, 2, 1)
         x = self.fc1(x)
